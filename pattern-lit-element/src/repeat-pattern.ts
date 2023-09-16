b6eac27e46f1ddc9d849841101e759307148ed60
MateoCastanoG <m.castano1@uniandes.edu.co>
2023-09-16 13:06:02 -0500

    :construction: Pattern skeleton

---
+++ pattern-lit-element/src/repeat-pattern.ts
@@ -0,0 +1,20 @@
+import type { SVGTemplateResult } from "lit";
+
+import { LitElement, html, svg } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+const createElement = (chars: string): SVGTemplateResult => svg`
+  <text dominant-baseline="hanging" font-family="monospace" font-size="24px">
+    ${chars}
+  </text>;
+`;
+
+@customElement("repeat-pattern")
+export class RepeatPattern extends LitElement {
+  @property({ type: String }) chars = "Hello, World!";
+  render() {
+    return html`<svg height="100%" width="100%">
+      ${createElement("Hello, World!")}
+    </svg> `;
+  }
+}
